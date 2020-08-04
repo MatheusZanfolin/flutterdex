216264adf976304e398ccd1ea52693d1134b6b4d
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2020-08-03 21:12:02 -0300

    Added remaining tabs' placeholders

--- lib/routes/route_favorites.dart
+++ lib/routes/route_favorites.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutterdex/widgets/widget_screen.dart';
 
 class FavoritesRoute extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => MainScreen(
+    title: 'Meus favoritos',
+    child: Container(),
+  );
 
 }
--- lib/routes/route_teams.dart
+++ lib/routes/route_teams.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutterdex/widgets/widget_screen.dart';
 
 class TeamsRoute extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => MainScreen(
+    title: 'Meus times',
+    child: Container(),
+  );
 
 }
